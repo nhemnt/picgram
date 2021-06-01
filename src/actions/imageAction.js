50b25dd79d1b3e870cdbfbb24536a7fed5a865ef
hemant negi <nhemnt@gmail.com>
2021-06-01 19:10:52 +0530

    remove unused methods

--- src/actions/imageAction.js
+++ src/actions/imageAction.js
@@ -3,48 +3,6 @@ import API from 'config/API';
 import { SET_IMAGES } from './types';
 import { imageURL } from 'config';
 
-import store from '../store';
-
-export const updateData = (dispatch, type, payload = []) => {
-    dispatch({
-        type,
-        payload
-    });
-};
-
-const getData = () => [...store.getState()?.imageReducer?.data];
-
-export const deleteComment = (id, commentIndex) => (dispatch) => {
-    const newData = getData().map((current) => {
-        return id === current.id
-            ? {
-                  ...current,
-                  comments: current.comments.filter((_, i) => i !== commentIndex)
-              }
-            : current;
-    });
-    updateData(dispatch, SET_IMAGES, newData);
-};
-export const addComment = (id, comment) => (dispatch) => {
-    const original = getData() || [];
-    const newData = original.map((current) => {
-        return current.id === id
-            ? { ...current, comments: [comment, ...current.comments] }
-            : current;
-    });
-    updateData(dispatch, SET_IMAGES, newData);
-};
-
-export const updateLike = (id) => (dispatch) => {
-    const original = getData() || [];
-    const newData = original.map((current) => {
-        return current.id === id
-            ? { ...current, likes: current.likes + (current.like ? -1 : 1), like: !current.like }
-            : current;
-    });
-    updateData(dispatch, SET_IMAGES, newData);
-};
-
 export const fetchImages = () => (dispatch) => {
     API.get(imageURL)
         .then((res) => {
